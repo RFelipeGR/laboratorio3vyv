9450678fd3199b3fa2cf367746c2ece07c7815e0
Cisco <cisco@udla-ec.int>
2026-07-01 20:59:32 -0500

    Probamos el test

--- src/test/java/hu/springbootrestfuljpa/books/BooksApplicationTests.java
+++ src/test/java/hu/springbootrestfuljpa/books/BooksApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BooksApplicationTests {
+public class BooksApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+    }
 }
